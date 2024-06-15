64831ad6559983b8e1fd84162c40f363f3a521de
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-15 02:33:13 -0500

    home page is now responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full items-center text-center text-6xl">
-        December 20th, 2023...
+    <main className="flex min-h-screen flex-col items-center justify-between py-8 max-sm:pb-24">
+      <div className="w-full items-center text-center text-4xl md:text-6xl">
+        June 24th, 2024...
       </div>
 
-      <div className="relative flex place-items-center">
+      <div className="relative hidden md:flex place-items-center ">
         <Image
           className=""
           src="/WizBangHeader.png"
@@ -18,7 +18,18 @@ export default function Home() {
         />
       </div>
 
-      <div className="text-center text-5xl">
+      <div className="relative hidden max-md:flex place-items-center">
+        <Image
+          className=""
+          src="/WizBangVertical.png"
+          alt="Wiz-Bang Boomers Logo"
+          width={800}
+          height={100}
+          priority
+        />
+      </div>
+
+      <div className="text-center text-3xl md:text-5xl">
         <p>
           711 Farm Road 1510 <br />
           Brookston, TX 75421
