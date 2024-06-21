eb97ee0f4ddd8b19851334b789c41ee573eec1dc
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-21 03:19:36 -0500

    updated colors

--- src/app/signboard/page.tsx
+++ src/app/signboard/page.tsx
@@ -7,13 +7,13 @@ export default function signboard() {
   return (
     <main className="flex flex-col items-center justify-between py-10 lg:py-8">
       <Image src="/WizBangHeader.png" alt="Wiz Bang Boomers header logo." priority width={350} height={350} />
-      <h1 className="font-semibold text-4xl md:text-5xl lg:text-6xl my-4">Categories</h1>
+      <h1 className="font-semibold text-4xl md:text-5xl my-4">Categories</h1>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
         {groupings.map((group) => (
-          <li key={group.name} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-wizLightBlue text-center shadow">
+          <li key={group.name} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-wizDarkBlue text-center shadow">
             <Link href={`/group/${group.group}`}>
               <div className="flex flex-1 flex-col p-8 md:p-10 lg:p-12">
-                <h3 className=" text-lg md:text-2xl font-medium text-gray-900">{group.name}</h3>
+                <h3 className=" text-lg md:text-2xl lg:text-3xl text-white">{group.name}</h3>
               </div>
             </Link>
           </li>
