306ce456bf13ad2215b5694b1e1fcebb13ca79a1
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-21 01:09:15 -0500

    filtering by category works now.

--- src/app/group/[group]/page.tsx
+++ src/app/group/[group]/page.tsx
@@ -8,25 +8,32 @@ export default function Group({ params }: { params: {group: string} }) {
   const groupName = params.group;
   const groupObj = groupings.find((obj) => obj.group === groupName);
   console.log(` I found group ${JSON.stringify(groupObj)}`);
+  const groupFireworks = fireworks.filter(firework => firework.group === groupName );
 
   return (
     <main className="flex flex-col items-center justify-between py-12">
-      <Image src="/WizBangHeader.png" alt="Wiz Bang Boomers header logo." width={350} height={350} />
+      <Image src="/WizBangHeader.png" alt="Wiz Bang Boomers header logo." priority width={350} height={350} />
       <div className="text-4xl py-4">
         {groupObj?.name}
       </div>
       <ul role="list" className="grid grid-cols-2 gap-4 md:grid-cols-3">
-        {fireworks.map((firework) => (
-          <li key={firework.sku} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-wizLightBlue text-center shadow">
+        {groupFireworks.map((firework) => (
+          <li key={firework.sku} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
             <Link href={`/video/${firework.sku}`}>
-              <div className="flex flex-1 flex-col p-8 md:p-10 lg:p-12">
+              <div className="flex flex-1 flex-col p-2 md:p-4">
+                <div className="flex items-end justify-end space-x-3">
+                  <span className="inline-flex flex-shrink-0 items-center rounded-full bg-indigo-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                    {firework.price}
+                  </span>
+                </div>
+                <Image src={`/images/${firework.sku}.png`} alt="" width={256} height={256}></Image>
                 <h3 className=" text-lg md:text-2xl font-medium text-gray-900">{firework.name}</h3>
               </div>
             </Link>
           </li>
         ))}
       </ul>
-      <div>
+      <div className="mt-4">
         Footer goes here.
       </div>
     </main>
