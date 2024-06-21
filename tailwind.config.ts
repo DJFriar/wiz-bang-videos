6f34911b8cc07e3214a9a0eeeab667349e248eae
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-20 23:25:00 -0500

    Added company colors as custom theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,20 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        'wizSilver': '#B9B9B9',
+        'wizBrightRed': '#B40404',
+        'wizDarkRed': '#7C0303',
+        'wizDarkestBlue': '#00152D',
+        'wizDarkBlue': '#002857',
+        'wizBlue': '#0464A8',
+        'wizLightBlue': '#0DDEFF',
+      }
+    },
+  },
   plugins: [],
 }
+
 export default config
