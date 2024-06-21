110b0ec4240c17ae9d535bff5a66060aea69cf36
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-20 23:25:48 -0500

    grid of fireworks is functional, but ugly.

--- src/app/group/[group]/page.tsx
+++ src/app/group/[group]/page.tsx
@@ -1,7 +1,8 @@
-'use client'
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-import { groupings } from "./data";
+import { fireworks, groupings } from "./data";
 
 export default function Group({ params }: { params: {group: string} }) {
   const groupName = params.group;
@@ -10,12 +11,21 @@ export default function Group({ params }: { params: {group: string} }) {
 
   return (
     <main className="flex flex-col items-center justify-between py-12">
-      <div className="text-4xl pb-2">
+      <Image src="/WizBangHeader.png" alt="Wiz Bang Boomers header logo." width={350} height={350} />
+      <div className="text-4xl py-4">
         {groupObj?.name}
       </div>
-      <div>
-        Fireworks grid goes here.
-      </div>
+      <ul role="list" className="grid grid-cols-2 gap-4 md:grid-cols-3">
+        {fireworks.map((firework) => (
+          <li key={firework.sku} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-wizLightBlue text-center shadow">
+            <Link href={`/video/${firework.sku}`}>
+              <div className="flex flex-1 flex-col p-8 md:p-10 lg:p-12">
+                <h3 className=" text-lg md:text-2xl font-medium text-gray-900">{firework.name}</h3>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
       <div>
         Footer goes here.
       </div>
--- src/app/signboard/page.tsx
+++ src/app/signboard/page.tsx
@@ -10,7 +10,7 @@ export default function signboard() {
       <h1 className="font-semibold text-4xl md:text-5xl lg:text-6xl my-4">Categories</h1>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
         {groupings.map((group) => (
-          <li key={group.name} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
+          <li key={group.name} className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-wizLightBlue text-center shadow">
             <Link href={`/group/${group.group}`}>
               <div className="flex flex-1 flex-col p-8 md:p-10 lg:p-12">
                 <h3 className=" text-lg md:text-2xl font-medium text-gray-900">{group.name}</h3>
