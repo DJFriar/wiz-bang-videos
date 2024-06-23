69078ec8aff6fbb480b68ad0b251775c4d7601f2
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-22 19:19:49 -0500

    Added clickable address to footer

--- src/app/components/WizFooter.tsx
+++ src/app/components/WizFooter.tsx
@@ -7,10 +7,16 @@ interface WizFooterProps {}
 const WizFooter: React.FC<WizFooterProps> = (props) => {
   return (
   <footer>
-    <div className="mx-auto overflow-hidden py-10 sm:py-24">
-      <div className="mt-10 md:mt-6 flex justify-center space-x-8">
+    <div className="mx-auto overflow-hidden pt-8">
+      <div className="text-center text-sm ">
+        <p className="text-gray-300">
+          <a href="https://www.google.com/maps/dir//711+FM1510,+Brookston,+TX+75421/@33.6605961,-95.6367631,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x864a561b233ba081:0x240ba1b24461dcfd!2m2!1d-95.6341828!2d33.6605961?entry=ttu" target="_blank">711 Farm Road 1510, Brookston, TX 75421</a> <br />
+          <a href="tel:8639492264">(863) WIZ-BANG</a>
+        </p>
+      </div>
+      <div className="mt-4 flex justify-center space-x-6">
         {WizNavigation.social.map((item) => (
-          <a key={item.name} href={item.href} target="_blank" className="text-gray-400 hover:text-gray-500">
+          <a key={item.name} href={item.href} target="_blank" className="text-gray-500 hover:text-gray-700">
             <span className="sr-only">{item.name}</span>
             <item.icon className="h-6 w-6" aria-hidden="true" />
           </a>
