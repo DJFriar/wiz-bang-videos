f9fc3f637b2d7bad6f5621f78eb163cb41c3bfa5
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-06-22 19:23:38 -0500

    spacing adjustment in footer

--- src/app/components/WizFooter.tsx
+++ src/app/components/WizFooter.tsx
@@ -9,9 +9,10 @@ const WizFooter: React.FC<WizFooterProps> = (props) => {
   <footer>
     <div className="mx-auto overflow-hidden pt-8">
       <div className="text-center text-sm ">
-        <p className="text-gray-300">
+        <p className="text-gray-300 pb-1">
           <a href="https://www.google.com/maps/dir//711+FM1510,+Brookston,+TX+75421/@33.6605961,-95.6367631,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x864a561b233ba081:0x240ba1b24461dcfd!2m2!1d-95.6341828!2d33.6605961?entry=ttu" target="_blank">711 Farm Road 1510, Brookston, TX 75421</a> <br />
-          <a href="tel:8639492264">(863) WIZ-BANG</a>
+        </p>
+        <p className="text-gray-300"><a href="tel:8639492264">(863) WIZ-BANG</a>
         </p>
       </div>
       <div className="mt-4 flex justify-center space-x-6">
